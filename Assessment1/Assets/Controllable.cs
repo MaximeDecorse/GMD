de66aa70e784778033c58631bd86c0586ad6c0df
Maxime Decorse <Maxime.Decorse@students.ittralee.ie>
2020-01-28 10:28:55 +0000

    Controllable and Touch controls class

---
+++ Assessment1/Assets/Controllable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour
+{
+    Renderer my_renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void select()
+    {
+        my_renderer.material.color = Color.yellow;
+    }
+
+    internal void unselect()
+    {
+        my_renderer.material.color = Color.white;
+    }
+
+
+}
---
+++ Assessment1/Assets/TouchControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+    float timer = 0;
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.touchCount > 0)
+        {
+            List<Touch> touches = new List<Touch>(Input.touches);
+            foreach (Touch touch in touches)
+            {
+                Ray ray = camera.ScreenPointToRay(touch.position);
+                Debug.DrawRay(ray.origin, 20 * ray.direction);
+                RaycastHit info_on_hit;
+            }
+        }
+            
+    }
+}
